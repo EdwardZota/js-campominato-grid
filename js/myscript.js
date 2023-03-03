67dd675d744ff576d95516e62d156b2268ecc5c3
Edward Zota <zotaedward@gmail.com>
2023-03-03 15:44:44 +0100

    bonus aggiunto ma da impostare il remove

--- js/myscript.js
+++ js/myscript.js
@@ -4,29 +4,94 @@ const play = document.getElementById('play');
 
 const title = document.getElementById('title');
 
-for( let i = 1; i <= 100; i++){
+const selected = document.getElementById('difficult');
 
-    const currentSquare = createSquare();
 
-    currentSquare.append(i);
 
-    currentSquare.addEventListener('click', function() {
-        this.classList.toggle('cyan');
-        console.log(`Hai scelto la casella numero: ${i}`)
-    }
-    )
 
-    containerDom.append(currentSquare);
-}
+const boxArray =[];
+
 
 play.addEventListener('click', function(){
     title.classList.add('d-none');
 
-    
+    if(boxArray.length == 0){
 
+        if(selected.value == "easy"){
+
+            for( let i = 1; i <= 100; i++){
+
+                const currentSquare = createSquare();
+
+                currentSquare.classList.add('box-1');
+            
+                currentSquare.append(i);
+            
+                currentSquare.addEventListener('click', function() {
+                    this.classList.toggle('cyan');
+                    console.log(`Hai scelto la casella numero: ${i}`);
+                }
+                )
+            
+                containerDom.append(currentSquare);
+                boxArray.push(currentSquare);
     
-}
+            }
+    
+            console.log(boxArray);
+        }else if(selected.value == "medium"){
+
+            for( let i = 1; i <= 81; i++){
+
+                const currentSquare = createSquare();
+
+                currentSquare.classList.add('box-2');
+            
+                currentSquare.append(i);
+            
+                currentSquare.addEventListener('click', function() {
+                    this.classList.toggle('cyan');
+                    console.log(`Hai scelto la casella numero: ${i}`)
+                }
+                )
+            
+                containerDom.append(currentSquare);
+                boxArray.push(currentSquare);
+    
+            }
+    
+            console.log(boxArray);
+        }else if(selected.value == "hard"){
+
+            for( let i = 1; i <= 49; i++){
+
+                const currentSquare = createSquare();
+
+                currentSquare.classList.add('box-3');
+            
+                currentSquare.append(i);
+            
+                currentSquare.addEventListener('click', function() {
+                    this.classList.toggle('cyan');
+                    console.log(`Hai scelto la casella numero: ${i}`)
+                }
+                )
+            
+                containerDom.append(currentSquare);
+                boxArray.push(currentSquare);
+    
+            }
+    
+            console.log(boxArray);
+        }
+
+
+
+    }
+
+    }
 )
+
 
 
 
